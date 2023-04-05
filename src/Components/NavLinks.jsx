0dc54814be2f0b45f4614505ca203f231cbc57c5
wageh zaietr <107758708+wagehzaietr@users.noreply.github.com>
2023-04-05 20:58:07 +0400

    Update NavLinks.jsx

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 import Logo from "../../public/logo.png";
 import { ShoppingCart } from "phosphor-react";
 import { Link } from "react-router-dom";
-import '../Components/navbar.css';
+import "../Components/navbar.css";
 
 const NavLinks = () => {
   return (
@@ -12,7 +12,9 @@ const NavLinks = () => {
         id="Navbar"
       >
         <div className="container">
+          <Link to='/'>
           <img src={Logo} alt="" width="80px" />
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -27,19 +29,25 @@ const NavLinks = () => {
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
               <a className="nav-link active" aria-current="page" href="#">
-              <Link to="/" className='link'> Home </Link>
+                <Link to="/" className="link">
+                  {" "}
+                  Home{" "}
+                </Link>
               </a>
               <a className="nav-link">
-              <Link to="/shop" className='link'> Shop </Link>
+                <Link to="/shop" className="link">
+                  {" "}
+                  Shop{" "}
+                </Link>
               </a>
               <a className="nav-link" href="#">
                 Pricing
               </a>
             </div>
           </div>
-              <Link to='/cart' >
-                <ShoppingCart size={28} color="#000000eb" className='cart'/>
-              </Link>
+          <Link to="/cart" id="mobile-cart">
+            <ShoppingCart size={28} color="#000000eb" className="cart" />
+          </Link>
         </div>
       </div>
     </>
@@ -48,10 +56,5 @@ const NavLinks = () => {
 
 export default NavLinks;
 
-{
-  /* <Link to="/"> Shop </Link>
-<Link to="/contact"> Contact </Link>
-<Link to="/cart">
-  <ShoppingCart size={32} />
-</Link> */
-}
+
+
